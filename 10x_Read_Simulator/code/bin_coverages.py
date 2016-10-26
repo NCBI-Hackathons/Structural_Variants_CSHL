aac210c6ee1b6f749c439e79d316d69dffe3ba96
Stephen Kelly <stevekm@users.noreply.github.com>
2016-10-26 19:29:24 +0000

    fix avg plot script

--- 10x_Read_Simulator/code/bin_coverages.py
+++ 10x_Read_Simulator/code/bin_coverages.py
@@ -7,6 +7,19 @@
 # This script will parse a depth of coverage file into binned regions
 # and calculate the average coverage per region
 
+# INPUT FILE FORMAT:
+# chr1  10003   6656    7998    8002    7996    7997    7998    7421    8000    7999    5620    7999    6887    7998
+# chr1  10004   7997    7999    8000    7997    7996    7997    7996    8000    8000    7786    8000    7998    7998
+# chr1  10005   8000    8000    8005    7998    7999    8002    7999    8002    8001    8000    8001    8001    8000
+# chr1  10006   8001    8004    8005    8003    8003    8005    8002    8003    8002    8000    8003    8002    8003
+# chr1  10007   8001    8004    8006    8003    8000    8005    8002    8001    7994    7998    8000    7992    8000
+# chr1  10008   7996    8000    8002    7994    7990    7999    7992    7985    7991    7995    7997    7991    7989
+# chr1  10009   8005    8007    8009    8006    8006    8007    8005    8006    8004    8003    8006    8005    8005
+
+# <chrom>\t<position>\t<coverage1>\t<coverage2>...<coverage_n>
+
+# EXAMPLE OUTPUT:
+
 import sys
 import csv
 import collections
@@ -29,10 +42,12 @@ def test_bins(value, start_bin, end_bin):
         sys.exit()
 
 def bin_file_regions(input_file, bin_size):
+    # ~~ SETUP ~~~ # 
     # dict to hold the coverage values
     chrom_bins = collections.defaultdict(lambda : collections.defaultdict(list))
     # keep track of bin counts while parsing file
     chrom_bin_counts = collections.defaultdict(lambda : collections.defaultdict(int))
+    # ~~~ READ FILE ~~ # 
     with open(input_file) as tsvin:
         tsvin = csv.reader(tsvin, delimiter='\t')
         for line in tsvin:
