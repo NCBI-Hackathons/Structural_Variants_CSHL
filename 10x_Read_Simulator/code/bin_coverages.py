4846168ae3f03b82b9d6c65714b78ecbd80521b7
Stephen Kelly <stevekm@users.noreply.github.com>
2016-10-26 01:38:07 +0000

    fix the bin script output

--- 10x_Read_Simulator/code/bin_coverages.py
+++ 10x_Read_Simulator/code/bin_coverages.py
@@ -13,36 +13,26 @@ import collections
 import average_coverages as av
 
 
+def make_bin_region(value, bin_size):
+    start_bin = int(value) - (int(value) % bin_size)
+    end_bin = start_bin + bin_size
+    # sanity check - make sure we binned correctly
+    test_bins(value, start_bin, end_bin)
+    return [start_bin, end_bin]
+
 def test_bins(value, start_bin, end_bin):
     if (int(value) < int(start_bin)) == True:
         print 'ERROR: value {} smaller than Start bin {}'.format(value, start_bin)
         sys.exit()
     if (int(value) > int(end_bin)) == True:
-        print 'ERROR: value {} greater than end bin {}'.format(value, end_bin)
+        print 'ERROR: value {} greater than End bin {}'.format(value, end_bin)
         sys.exit()
 
-
-
-input_file = sys.argv[1]
-chrom_sizes_file = "code/hg38_chrom_sizes.txt"
-bin_size = 1000
-chrom_coverage_counts = collections.defaultdict(int)
-# chrom_sizes = collections.defaultdict(int)
-chrom_bins = collections.defaultdict(lambda : collections.defaultdict(list))
-# keep track of bins while parsing file
-chrom_bin_counts = collections.defaultdict(lambda : collections.defaultdict(int))
-
-
-if __name__ == '__main__':
-    # # create bins from chom sizes # don't use this
-    # with open(chrom_sizes_file) as chrom_file:
-    #     chrom_file = csv.reader(chrom_file, delimiter='\t')
-    #     for line in chrom_file:
-    #         print line
-    #         chrom = line.pop(0)
-    #         size = line.pop(0)
-    #         chrom_bins[chrom] = range(1, int(size))
-    # print chrom_bins
+def bin_file_regions(input_file, bin_size):
+    # dict to hold the coverage values
+    chrom_bins = collections.defaultdict(lambda : collections.defaultdict(list))
+    # keep track of bin counts while parsing file
+    chrom_bin_counts = collections.defaultdict(lambda : collections.defaultdict(int))
     with open(input_file) as tsvin:
         tsvin = csv.reader(tsvin, delimiter='\t')
         for line in tsvin:
@@ -51,23 +41,20 @@ if __name__ == '__main__':
             # convert remaining values to int
             line = map(int, line)
             # set position bins
-            position_start_bin = int(position) - (int(position) % bin_size)
-            position_end_bin = position_start_bin + bin_size
+            position_start_bin, position_end_bin = make_bin_region(position, bin_size)
             position_bin = str(position_start_bin) + '-' + str(position_end_bin)
-            # sanity check - make sure we binned correctly
-            test_bins(position, position_start_bin, position_end_bin)
-            # sanity check - make sure we don't add too many more entries to the bin
+            # sanity check - make sure we don't add too many entries to the bin
             chrom_bin_counts[chrom][position_bin] += 1 
             if chrom_bin_counts[chrom][position_bin] <= bin_size:
                 if not chrom_bins[chrom][position_bin]:
                     chrom_bins[chrom][position_bin] = line
                 else :
                     chrom_bins[chrom][position_bin] = [x + y for x, y in zip(chrom_bins[chrom][position_bin], line)]
+                    # chrom_bins[chrom][position_bin] = [sum(x) for x in zip(*chrom_bins[chrom][position_bin])]
             else :
                 print 'ERROR: bin exceeded!'
                 print chrom, position, position_bin, chrom_bin_counts[chrom][position_bin]
                 sys.exit()
-                # chrom_bins[chrom][position_bin] = [sum(x) for x in zip(*chrom_bins[chrom][position_bin])]
             # ~~~~~~ # # ~~~~~~ # # ~~~~~~ #
             # include shifted bin window
             # position_starthalf_bin = int(position) - (int(position) % bin_size) - (bin_size / 2)
@@ -75,13 +62,15 @@ if __name__ == '__main__':
             # position_halfbin = str(position_starthalf_bin) + '-' + str(position_endhalf_bin)
             # test_bins(position, position_starthalf_bin, position_endhalf_bin)
             # ~~~~~~ # # ~~~~~~ # # ~~~~~~ # 
-            print chrom, position, position_bin #, position_halfbin
-    #
+            # print chrom, position, position_bin 
+        return chrom_bins
+
+input_file = sys.argv[1]
+bin_size = 1000
+
+if __name__ == '__main__':
+    chrom_bins = bin_file_regions(input_file, bin_size)
     for chrom, statsdict in chrom_bins.iteritems():
-        for statstat in statsdict.iteritems():
-            print chrom, statstat
-    #
-    # chrom_average_coverages = av.genome_avg_coverages(input_file)
-    # print, for stdout piping
-    # for chrom in sorted(chrom_average_coverages.keys()):
-    #     print chrom + '\t' + '\t'.join(map(str,chrom_average_coverages[chrom]))
+        for region, values in statsdict.iteritems():
+            # print chrom, statstat
+            print chrom + '\t' + '\t'.join(map(str,region.split('-'))) + '\t'.join(map(str,values))
